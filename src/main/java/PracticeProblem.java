964dfddb60520159a32bc15fe589728a78d8fa4c
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-19 19:06:43 +0000

    first ver

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,39 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+public static int lettersToFive(String word, char letter){
+int count = 0;
+int num =0;
+while (count <5){
+
+	if (word.charAt(num) == letter){
+		count ++;
+	}
+	num++;
+}return num;
+}
+
+public static int countLetter(String word, char letter){
+	int count = 0;
+		for (int num = 0; num < word.length();num++){
+		if (word.charAt(num) == letter){
+			count ++;
+		}
+	}
+	return count;
+}
+
+public static int oddSum(int num, int num2){
+	int max = Math.max(num, num2);
+	int sum = 0;
+	for (int count = Math.min(num, num2); count <= max; count++){
+if (count%2 != 0){
+sum +=count;
+}
+
+	}
+	return sum;
+}
 }
